b9200c0761de29dad8a94345e1afb7b2267aeb29
Gode King <GengGode@outlook.com>
2022-03-16 00:17:39 +0800

    feat(翻译): 完善翻译的词条
    为聊天框中出现的一些主要中文词条进行了翻译

--- Genshin_TianLi_TeamVoice/Genshin_TianLi_TeamVoice_zh.ts
+++ Genshin_TianLi_TeamVoice/Genshin_TianLi_TeamVoice_zh.ts
@@ -17,12 +17,12 @@
         <location filename="Genshin_TianLi_TeamVoice.cpp" line="291"/>
         <source>tr_ShiLiao:</source>
         <oldsource>tr_ShiLiao</oldsource>
-        <translation type="unfinished">私聊：</translation>
+        <translation>私聊：</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.cpp" line="295"/>
         <source>tr_FangJian:</source>
-        <translation type="unfinished"></translation>
+        <translation>房间：</translation>
     </message>
 </context>
 <context>
@@ -30,93 +30,94 @@
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="14"/>
         <source>Genshin_TianLi_TeamVoice</source>
+        <oldsource>tr_Genshin_TianLi_TeamVoice</oldsource>
         <translation>原神天理-队伍语音</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="76"/>
         <source>开始拾音</source>
-        <translation type="unfinished"></translation>
+        <translation>开始拾音</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="99"/>
         <source>开始播音</source>
-        <translation type="unfinished"></translation>
+        <translation>开始播音</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="112"/>
         <source>开始连接</source>
-        <translation type="unfinished"></translation>
+        <translation>开始连接</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="125"/>
         <source>停止连接</source>
-        <translation type="unfinished"></translation>
+        <translation>停止连接</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="177"/>
         <source>输入设备</source>
-        <translation type="unfinished"></translation>
+        <translation>输入设备</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="190"/>
         <source>输出设备</source>
-        <translation type="unfinished"></translation>
+        <translation>输出设备</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="203"/>
         <source>服务器url</source>
-        <translation type="unfinished"></translation>
+        <translation>服务器url</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="216"/>
         <source>自己的UID</source>
-        <translation type="unfinished"></translation>
+        <translation>自己的UID</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="229"/>
         <source>停止拾音</source>
-        <translation type="unfinished"></translation>
+        <translation>停止拾音</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="242"/>
         <source>停止播音</source>
-        <translation type="unfinished"></translation>
+        <translation>停止播音</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="265"/>
         <source>队伍UID</source>
-        <translation type="unfinished"></translation>
+        <translation>队伍UID</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="298"/>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="383"/>
         <source>发送消息</source>
-        <translation type="unfinished"></translation>
+        <translation>发送消息</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="311"/>
         <source>退出队伍</source>
-        <translation type="unfinished"></translation>
+        <translation>退出队伍</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="344"/>
         <source>私聊UID</source>
-        <translation type="unfinished"></translation>
+        <translation>私聊UID</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="357"/>
         <source>私聊</source>
-        <translation type="unfinished"></translation>
+        <translation>私聊</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="370"/>
         <source>队伍</source>
-        <translation type="unfinished"></translation>
+        <translation>队伍</translation>
     </message>
     <message>
         <location filename="Genshin_TianLi_TeamVoice.ui" line="396"/>
         <source>加入队伍</source>
-        <translation type="unfinished"></translation>
+        <translation>加入队伍</translation>
     </message>
 </context>
 </TS>
